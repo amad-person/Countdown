fc4a2236e43b43e7b69ec08e82033b471f0f77a4
amad-person <e0072565@u.nus.edu>
2019-02-02 18:59:58 +0800

    Add popup for delete button

--- src/components/CountdownCardList/CountdownCard/CountdownCard.js
+++ src/components/CountdownCardList/CountdownCard/CountdownCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card, Button} from "semantic-ui-react";
+import {Card, Button, Popup} from "semantic-ui-react";
 import moment from 'moment';
 import './CountdownCard.css';
 
@@ -15,8 +15,12 @@ const countdownCard = props => {
 
     return (
         <Card className="CountdownCard" style={style}>
-            <Button id="deleteButton" icon="trash alternate outline"
-                    onClick={props.deleteHandler}
+            <Popup
+                trigger={<Button id="deleteButton" icon="trash alternate outline"
+                        onClick={props.deleteHandler}/>}
+                content="Delete this event"
+                position="bottom right"
+                verticalOffset={-10}
             />
             <h3 id="eventName">{props.eventName}</h3>
             <h2 id="numDays">{futureDate.diff(currentDate, 'days')}</h2>
