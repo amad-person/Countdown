3e0abf1f4605bb0d60d4829e217f53291d630b47
amad-person <e0072565@u.nus.edu>
2019-02-03 15:29:49 +0800

    Updates

---
+++ precache-manifest.6598a9e8e248b39480e402db0d5f4926.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e9a6cb21843ba0d0916",
+    "url": "/Countdown/static/css/main.cf0b4d38.chunk.css"
+  },
+  {
+    "revision": "2e9a6cb21843ba0d0916",
+    "url": "/Countdown/static/js/main.2e9a6cb2.chunk.js"
+  },
+  {
+    "revision": "516a37df6dd508e88c1a",
+    "url": "/Countdown/static/css/1.6b7378ad.chunk.css"
+  },
+  {
+    "revision": "516a37df6dd508e88c1a",
+    "url": "/Countdown/static/js/1.516a37df.chunk.js"
+  },
+  {
+    "revision": "8912084071cf63386951",
+    "url": "/Countdown/static/js/runtime~main.89120840.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Countdown/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Countdown/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Countdown/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Countdown/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Countdown/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Countdown/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Countdown/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Countdown/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Countdown/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Countdown/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Countdown/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Countdown/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Countdown/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Countdown/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Countdown/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Countdown/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "dcec47051e97538af876b09d30e39e2c",
+    "url": "/Countdown/index.html"
+  }
+];
