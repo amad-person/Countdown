7584dcdbf61a348495c0ca0acfd32a639907d2ad
amad-person <e0072565@u.nus.edu>
2019-02-03 15:11:15 +0800

    Add form validation

--- src/containers/CountdownForm/CountdownForm.js
+++ src/containers/CountdownForm/CountdownForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Form } from "semantic-ui-react";
+import { Form, Label } from "semantic-ui-react";
+import moment from "moment";
 import "./CountdownForm.css";
 
 class CountdownForm extends Component {
@@ -9,7 +10,14 @@ class CountdownForm extends Component {
     this.defaultState = {
       eventName: "",
       eventDate: "",
-      randomBgColor: ""
+      randomBgColor: "",
+      formErrors: {
+        eventName: "",
+        eventDate: ""
+      },
+      eventNameValid: false,
+      eventDateValid: false,
+      formValid: false
     };
 
     this.state = this.defaultState;
@@ -17,9 +25,58 @@ class CountdownForm extends Component {
 
   handleChange = event => {
     const { name, value } = event.target;
+    this.setState(
+      {
+        [name]: value
+      },
+      () => {
+        this.validateField(name, value);
+      }
+    );
+  };
 
+  hasError(error) {
+    return error.length > 0;
+  }
+
+  validateField = (fieldName, value) => {
+    let formErrors = this.state.formErrors,
+      eventNameValid = this.state.eventNameValid,
+      eventDateValid = this.state.eventDateValid;
+    switch (fieldName) {
+      case "eventName":
+        value = value.trim();
+        eventNameValid = value.length > 0;
+        formErrors.eventName = eventNameValid
+          ? ""
+          : "Please enter a valid event name";
+        break;
+      case "eventDate":
+        let currentDate = moment(),
+          inputDate = moment(value);
+        eventDateValid = inputDate.diff(currentDate, "days") >= 0;
+        formErrors.eventDate = eventDateValid
+          ? ""
+          : "Please enter a date in the future";
+        break;
+      default:
+        break;
+    }
+
+    this.setState(
+      {
+        formErrors: formErrors,
+        eventNameValid: eventNameValid,
+        eventDateValid: eventDateValid
+      },
+      this.validateForm
+    );
+  };
+
+  validateForm = () => {
+    const formValid = this.state.eventNameValid && this.state.eventDateValid;
     this.setState({
-      [name]: value
+      formValid: formValid
     });
   };
 
@@ -52,26 +109,53 @@ class CountdownForm extends Component {
     return (
       <Form className="Form" onSubmit={this.handleSubmit}>
         <Form.Group>
-          <Form.Field width={8}>
+          <Form.Field
+            error={this.hasError(this.state.formErrors.eventName)}
+            width={8}
+          >
             <label>Event</label>
             <input
-              type="text"
               name="eventName"
-              value={this.state.eventName}
+              onBlur={() =>
+                this.validateField("eventName", this.state.eventName)
+              }
               onChange={this.handleChange}
+              type="text"
+              value={this.state.eventName}
             />
+            {this.hasError(this.state.formErrors.eventName) ? (
+              <Label basic color="red" pointing>
+                {this.state.formErrors.eventName}
+              </Label>
+            ) : null}
           </Form.Field>
-          <Form.Field width={8}>
+          <Form.Field
+            error={this.hasError(this.state.formErrors.eventDate)}
+            width={8}
+          >
             <label>Date</label>
             <input
-              type="date"
               name="eventDate"
-              value={this.state.eventDate}
+              onBlur={() =>
+                this.validateField("eventDate", this.state.eventDate)
+              }
               onChange={this.handleChange}
+              type="date"
+              value={this.state.eventDate}
             />
+            {this.hasError(this.state.formErrors.eventDate) ? (
+              <Label basic color="red" pointing>
+                {this.state.formErrors.eventDate}
+              </Label>
+            ) : null}
           </Form.Field>
         </Form.Group>
-        <Form.Button id="submitButton" primary type="submit">
+        <Form.Button
+          disabled={!this.state.formValid}
+          id="submitButton"
+          primary
+          type="submit"
+        >
           Add Event!
         </Form.Button>
       </Form>
